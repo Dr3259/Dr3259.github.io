f83127f960edad83eca4a8a7813d8ee4b5aec269
窦瑞 <d1844532477r@gmail.com>
2025-08-26 06:38:44 +0000

    第二行的第一个小球，运动到第一行的第二个小球的位置，在运动到第一行第一个小球，在运动到第二行第一个小球的位置，小球的一次周期运动结束，匀速，整个

--- src/app/health/astigmatism-exercise/page.tsx
+++ src/app/health/astigmatism-exercise/page.tsx
@@ -109,6 +109,25 @@ const DotMatrixExercise: React.FC = () => {
   }
 
   return (
+    <>
+      <style>
+        {`
+          @keyframes point-to-point-triangle {
+            0% {
+              transform: translate(0, 0);
+            }
+            33.33% {
+              transform: translate(calc(0.5 * (0.375rem + 0.5rem)), calc(-1 * (0.375rem + 0.5rem)));
+            }
+            66.66% {
+              transform: translate(calc(-0.5 * (0.375rem + 0.5rem)), calc(-1 * (0.375rem + 0.5rem)));
+            }
+            100% {
+              transform: translate(0, 0);
+            }
+          }
+        `}
+      </style>
       <div 
         className="w-full h-80 bg-black rounded-lg overflow-hidden"
       >
@@ -125,13 +144,16 @@ const DotMatrixExercise: React.FC = () => {
                     key={dot.id}
                     className="bg-white rounded-full w-[6px] h-[6px] m-auto"
                     style={{
-                      transform: isOddRow ? 'translateX(calc(50% + (0.5rem / 2)))' : 'translateX(0)',
+                      transform: isOddRow ? `translateX(calc(0.5 * (0.375rem + 0.5rem)))` : 'translateX(0)',
+                      animation: `point-to-point-triangle 6s linear infinite`,
+                      animationDelay: `${dot.delay}s`,
                     }}
                   />
               )
             })}
         </div>
       </div>
+    </>
   );
 };
 
@@ -219,3 +241,5 @@ export default function AstigmatismExercisePage() {
     </div>
   );
 }
+
+    
