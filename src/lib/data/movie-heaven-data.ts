65bca961cf91ffa2bd3db83506afa83e907e754b
窦瑞 <d1844532477r@gmail.com>
2025-08-27 11:07:27 +0000

    [ {"title": "2025年悬疑《私家侦探》HD国粤双语中英双字", "url": "https://dydytt.net/html/g

--- src/lib/data/movie-heaven-data.ts
+++ src/lib/data/movie-heaven-data.ts
@@ -11,17 +11,178 @@ export interface MovieHeavenItem {
 // Paste your JSON data here
 export const movieHeavenData: MovieHeavenItem[] = [
   {
-    "title": "一个伟大时代的缩影",
-    "downloadUrl": "ftp://ygdy8:ygdy8@yg76.dydytt.net:6023/阳光电影www.ygdy8.com.一个伟大时代的缩影.BD.1080P.国语中字.mkv",
-    "rating": "7.9",
-    "tags": "剧情 / 传记 / 历史",
-    "shortIntro": "影片以真实历史事件为基础，讲述了一群普通人在时代洪流中的奋斗与选择，展现了他们的理想、爱情与挣扎。"
+    "title": "2025年悬疑《私家侦探》HD国粤双语中英双字",
+    "downloadUrl": "magnet:?xt=urn:btih:45a1c5c1cd104416fc2144543e98ae6271897f7f&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e7%a7%81%e5%ae%b6%e4%be%a6%e6%8e%a2.2025.HD.4K.%e5%9b%bd%e7%b2%a4%e5%8f%8c%e8%af%ad%e4%b8%ad%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
   },
   {
-    "title": "神秘代码",
-    "downloadUrl": "ftp://ygdy8:ygdy8@yg76.dydytt.net:6023/阳光电影www.ygdy8.com.神秘代码.BD.1080P.中英双字.mkv",
-    "rating": "8.5",
-    "tags": "科幻 / 悬疑 / 惊悚",
-    "shortIntro": "一个大学教授无意间发现了一个来自过去的时间胶囊，里面的一串数字竟精准预言了过去50年发生的重大灾难。"
+    "title": "2025年动作《三恶人/一身邪气》BD中英双字",
+    "downloadUrl": "magnet:?xt=urn:btih:1e83736d23c5d3b8e2414c942ea855c20743cdf8&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e4%b8%89%e6%81%b6%e4%ba%ba.2025.HD.1080P.%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年喜剧《高尔夫球也疯狂2/球场古惑仔2》BD中英双字",
+    "downloadUrl": "magnet:?xt=urn:btih:ffb6748c054e99d44bc445805f8d49adf01150fa&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e9%ab%98%e5%b0%94%e5%a4%ab%e7%90%83%e4%b9%9f%e7%96%af%e7%8b%822.2025.BD.1080P.%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年喜剧爱情《多幸运遇见你》HD国语中字",
+    "downloadUrl": "magnet:?xt=urn:btih:5dc88e4de0b4b4976d0a9555ff805d2696f8db86&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e5%a4%9a%e5%b9%b8%e8%bf%90%e9%81%87%e8%a7%81%e4%bd%a0.2025.HD.4K.%e5%9b%bd%e8%af%ad%e4%b8%ad%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2024年惊悚《伊甸/物种起源》BD中英双字",
+    "downloadUrl": "magnet:?xt=urn:btih:d22cc084773e9cbf81501f543e047b0271a03feb&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e4%bc%8a%e7%94%b8.2024.BD.1080P.%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年科幻惊悚《梅根2.0》HD中英双字",
+    "downloadUrl": "magnet:?xt=urn:btih:914666a6532ee532dc9305ed3749f0fbc7a20941&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e6%a2%85%e6%a0%b92.0.2025.HD.1080P.%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年剧情《钻石照耀钟鼓楼》HD国语中字",
+    "downloadUrl": "magnet:?xt=urn:btih:135fb367bd26efe39ec5d370d3698e4470aeb94e&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e9%92%bb%e7%9f%b3%e7%85%a7%e8%80%80%e9%92%9f%e9%bc%93%e6%a5%bc.2025.HD.4K.%e5%9b%bd%e8%af%ad%e4%b8%ad%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年动作科幻《侏罗纪世界：重生》HD中英双字",
+    "downloadUrl": "magnet:?xt=urn:btih:ddd0096c8e09e849807d0833b106d2a468581c61&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e4%be%8f%e7%bd%97%e7%ba%aa%e4%b8%96%e7%95%8c%ef%bc%9a%e9%87%8d%e7%94%9f.2025.HD.1080P.%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年科幻惊悚《惊变28年》BD中英双字",
+    "downloadUrl": "magnet:?xt=urn:btih:845e26715df66f1bd5ceb973e61bbac27852bda5&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e6%83%8a%e5%8f%9828%e5%b9%b4.2025.BD.1080P.%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年动作奇幻《新·驯龙高手》BD国英双语双字",
+    "downloadUrl": "magnet:?xt=urn:btih:10cc5f05243f8927dc35daa36525e29baa42e310&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e6%96%b0%e9%a9%af%e9%be%99%e9%ab%98%e6%89%8b.2024.BD.1080P.%e5%9b%bd%e8%8b%b1%e5%8f%8c%e8%af%ad%e5%8f%8c%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年剧情犯罪《缉盗》HD国语中字",
+    "downloadUrl": "magnet:?xt=urn:btih:ccfa23a443e4350ce1049e30767506df0fa2f257&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e7%bc%89%e7%9b%97.2025.HD.4K.%e5%9b%bd%e8%af%ad%e4%b8%ad%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年剧情《但愿人长久》HD国语中英双字",
+    "downloadUrl": "magnet:?xt=urn:btih:3727b2932105d6f7d5855241667f4eb815b0caa4&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e4%bd%86%e6%84%bf%e4%ba%ba%e9%95%bf%e4%b9%85.2025.HD.1080P.%e5%9b%bd%e8%af%ad%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年动作喜剧《唐探1900》BD国语中字",
+    "downloadUrl": "magnet:?xt=urn:btih:368ad2a24368547f426a0c05b2d99b73bdbce370&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e5%94%90%e6%8e%a21900.2025.BD.1080P.%e5%9b%bd%e8%af%ad%e4%b8%ad%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年动画《海底小纵队：海啸大危机》HD国语中字",
+    "downloadUrl": "magnet:?xt=urn:btih:53e42878e863187ec2f05ec6865c919e9bb4a8e3&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e6%b5%b7%e5%ba%95%e5%b0%8f%e7%ba%b5%e9%98%9f%ef%bc%9a%e6%b5%b7%e5%95%b8%e5%a4%a7%e5%8d%b1%e6%9c%ba.2025.HD.1080P.%e5%9b%bd%e8%af%ad%e4%b8%ad%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年动作科幻《雷霆特攻队*》BD国英双语中字",
+    "downloadUrl": "magnet:?xt=urn:btih:39d7614c650ac2a16f9885cafebf29271500845e&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e9%9b%b7%e9%9c%86%e7%89%b9%e6%94%bb%e9%98%9f.2025.BD.1080P.%e5%9b%bd%e8%8b%b1%e5%8f%8c%e8%af%ad%e4%b8%ad%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年动作奇幻喜剧《新·驯龙高手》HD中英双字",
+    "downloadUrl": "magnet:?xt=urn:btih:f594bb1f9f2dc0cd2bb5655da820c843d45f85dd&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e6%96%b0%c2%b7%e9%a9%af%e9%be%99%e9%ab%98%e6%89%8b.2025.HD.1080P.%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年动画奇幻《时间之子》HD国语中字",
+    "downloadUrl": "magnet:?xt=urn:btih:d70f7d6196b88f80086f04b6a08d6205071e79a1&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e6%97%b6%e9%97%b4%e4%b9%8b%e5%ad%90.2025.HD.1080P.%e5%9b%bd%e8%af%ad%e4%b8%ad%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年剧情喜剧《我的牛津岁月》BD中英双字",
+    "downloadUrl": "magnet:?xt=urn:btih:9b27d647e9aa13d09f82d166cf83378bd5802ff8&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e6%88%91%e7%9a%84%e7%89%9b%e6%b4%a5%e5%b2%81%e6%9c%88.2025.BD.1080P.%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2024年惊悚《拿针的女孩/道格玛杀手》HD中字",
+    "downloadUrl": "magnet:?xt=urn:btih:5719291f3770c5dc7223997c8104425546c668dd&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e6%8b%bf%e9%92%88%e7%9a%84%e5%a5%b3%e5%ad%a9.2024.HD.1080P.%e4%b8%ad%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年喜剧爱情《物质主义者》HD中英双字",
+    "downloadUrl": "magnet:?xt=urn:btih:20b41d4f32263706003899d50982f12fa0a91882&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e7%89%a9%e8%b4%a8%e4%b8%bb%e4%b9%89%e8%80%85.2025.HD.1080P.%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年剧情恐怖《黑修女们》BD韩语中字",
+    "downloadUrl": "magnet:?xt=urn:btih:e8882225c8429e5c6cd4804541923c297db321f3&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e9%bb%91%e4%bf%ae%e5%a5%b3%e4%bb%ac.2025.BD.1080P.%e9%9f%a9%e8%af%ad%e4%b8%ad%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年喜剧爱情《非标准恋爱》HD国语中英双字",
+    "downloadUrl": "magnet:?xt=urn:btih:e8b9c8da7c1cc811a74dc8595c51b6bd520150a9&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e9%9d%9e%e6%a0%87%e5%87%86%e6%81%8b%e7%88%b1.2025.HD.4K.%e5%9b%bd%e8%af%ad%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2024年剧情《企鹅课》BD中英双字",
+    "downloadUrl": "magnet:?xt=urn:btih:fbcf6873be9a38a990edfeabe6fb885162a15a8b&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e4%bc%81%e9%b9%85%e8%af%be.2024.BD.1080P.%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年动画奇幻《哪吒2之魔童闹海》HD国语中字",
+    "downloadUrl": "magnet:?xt=urn:btih:4795f5bea979e3ac2c5c96c9c0f80fde49fadace&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e5%93%aa%e5%90%92%e4%b9%8b%e9%ad%94%e7%ab%a5%e9%97%b9%e6%b5%b7.2025.HD.1080P.%e5%9b%bd%e8%af%ad%e4%b8%ad%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
+  },
+  {
+    "title": "2025年动作喜剧《星际宝贝史迪奇》BD英国粤三语双字",
+    "downloadUrl": "magnet:?xt=urn:btih:8fee23550fd2476b41bdb7688d648e9f59b5a411&dn=%e9%98%b3%e5%85%89%e7%94%b5%e5%bd%b1dygod.org.%e6%98%9f%e9%99%85%e5%ae%9d%e8%b4%9d%e5%8f%b2%e8%bf%aa%e5%a5%87.2025.BD.1080P.%e5%9b%bd%e7%b2%a4%e8%8b%b1%e4%b8%89%e8%af%ad%e4%b8%ad%e8%8b%b1%e5%8f%8c%e5%ad%97.mkv&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce",
+    "rating": "暂无评分",
+    "tags": "未知",
+    "shortIntro": "暂无简介"
   }
-];
+]
