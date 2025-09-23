406607eca20ac488287c5fea2e272ad2f2246b19
窦瑞 <d1844532477r@gmail.com>
2025-09-23 21:12:22 +0000

    一个遮挡bug，最左侧的歌单被选中的时候，被选中的效果 左边的那条线被遮挡了，能理解吗

--- src/components/playlist-grid.tsx
+++ src/components/playlist-grid.tsx
@@ -48,8 +48,8 @@ export const PlaylistGrid: React.FC<PlaylistGridProps> = ({
         </h2>
       </div>
       
-      <ScrollArea className="flex-1 w-full -mx-1">
-        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3 p-1 pb-4">
+      <ScrollArea className="flex-1 w-full">
+        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3 px-1 pb-4">
           {/* 现有歌单卡片 */}
           {playlists.map((playlist) => (
             <PlaylistCard
@@ -75,7 +75,7 @@ export const PlaylistGrid: React.FC<PlaylistGridProps> = ({
                   : undefined
               }
               onDelete={playlist.id !== 'all-music' ? () => onDeletePlaylist(playlist.id) : undefined}
-              onDrop={(trackId) => onTrackDrop(trackId, playlist.id)}
+              onTrackDrop={(trackId) => onTrackDrop(trackId, playlist.id)}
             />
           ))}
           
